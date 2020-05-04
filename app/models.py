ea93a91feee4a390c720892b76325a5d18821f03
Ji Hua <michaelhua1993@foxmail.com>
2020-05-03 23:18:57 -0400

    add user login

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 
-from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+from app import db, login
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -11,6 +14,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Vote(db.Model):
@@ -23,3 +32,8 @@ class Vote(db.Model):
 
     def __repr__(self):
         return f'<Vote {self.seat} votes for {self.vote_for} at round {self.round_id}>'
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
