be9337ff6ee9df2e5c2b05a1cd87ee0df2683f9c
Ji Hua <michaelhua1993@foxmail.com>
2020-05-05 19:56:57 -0400

    change flask shell

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import User, Vote
+from app.models import User, Vote, Game
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Vote': Vote}
+    return {'db': db, 'User': User, 'Vote': Vote, 'Game': Game}
