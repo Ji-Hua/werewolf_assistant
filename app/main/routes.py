0d55c2e7229dc58664ca4ed93b81dc0fdc635069
Ji Hua <michaelhua1993@foxmail.com>
2020-09-13 23:02:27 -0400

    add error handling

--- app/main/routes.py
+++ app/main/routes.py
@@ -33,10 +33,11 @@ def index():
     return render_template('index.html', title='首页', form=form)
 
 
-@bp.errorhandler(404)
+# TODO: add logging and email
+@bp.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
 
-@bp.errorhandler(500)
+@bp.app_errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('errors/500.html'), 500
