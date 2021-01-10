da20cd2d802188a1cff8e06e7df5573d5d1a4de8
Ji Hua <michaelhua1993@foxmail.com>
2021-01-10 15:40:09 -0500

    redesign index page and game

--- app/main/routes.py
+++ app/main/routes.py
@@ -14,23 +14,8 @@ from app.models import User, Room, Player
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 @login_required
-def index():
-    form = CreateGameForm()
-    if current_user.is_authenticated:
-        if form.validate():
-            if form.create_game.data:
-                next_page = url_for('game.setup')
-                return redirect(next_page)
-            if form.enter_game.data:
-                # TODO: should validate room_name here
-                room = Room.query.filter_by(name=form.room_name.data).first()
-                player = Player(user_id=current_user.id, room_id=room.id, is_host=False)
-                db.session.add(player)
-                db.session.commit()
-                next_page = url_for('game.room', room_name=room.name)
-            return redirect(next_page)
-        
-    return render_template('index.html', title='首页', form=form)
+def index():      
+    return render_template('index.html', title='首页')
 
 
 # TODO: add logging and email
